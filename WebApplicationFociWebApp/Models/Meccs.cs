0319e0deaac1b356481bb34c9f34b05f64542053
barizs.marton.daniel <barizs.marton.daniel@BURG-1E03-13>
2024-09-16 13:06:45 +0200

    Add project files.

---
+++ WebApplicationFociWebApp/Models/Meccs.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplicationFociWebApp.Models
+{
+    public class Meccs
+    {
+        public int Id {  get; set; }
+        public int Fordulo { get; set; }
+        public string? HazaiCsapatNeve { get; set; }
+        public string? VendegCsapadNeve { get; set; }
+        public int HazaiGolok {  get; set; }
+        public int VendegGolok { get; set; }
+        public int HazaiFelido { get; set; }
+        public int VendegFelido { get; set; }
+
+       
+    }
+
+}
